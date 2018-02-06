d2262fbd67a1a28abca41ee1722da648c36b03f9
Alexander Allen <alexander.ryan.allen@gmail.com>
2018-02-05 23:01:40 -0800

    messign with mongo

--- swearjar.js
+++ swearjar.js
@@ -1,11 +1,18 @@
 var SlackBot = require('slackbots');
 var Profanity = require('profanity-util');
+var MongoClient = require('mongodb').MongoClient;
 
 var settings = {
     token: process.env.BOT_API_KEY,
     name: "Swear Jar"
 };
 
+// TODO: As with everything else, organize
+var dbHost = process.env.DB_HOST;
+var dbUser = process.env.DB_USER;
+var dbPass = process.env.DB_PASS;
+var dbConnection = "mongodb://" + dbUser + ":" + encodeURIComponent(dbPass) + "@" + dbHost + ":10255/?ssl=true&replicaSet=globaldb";
+ 
 var bot = new SlackBot(settings);
 
 // TODO: Move these to a prototype or something
@@ -22,19 +29,38 @@ _getNaughtyWords = function(message) {
     return Profanity.check(message.text);
 };
 
+// TODO: This is dumb, I should really just pre-populate the word list
+_updateWordList = function(word) {
+    MongoClient.connect(dbConnection, function(err,client) {
+        if (err) return;
+
+        var db = client.db("swear_jar");
+        db.collection("bad_words").updateOne(
+            { text: word },
+            {
+                $inc: { count: 1},
+                $setOnInsert: { text: word, count: 1 }
+            },
+            {upsert: true }
+        );
+
+        client.close();
+    });
+};
+
 bot.on('start', function()
 {
-    bot.postMessageToChannel("swearjartest", "its on");
 });
 
 bot.on('message', function(data) {
     console.log(data);
 
-    if (_isChatMessage(data) && _isChannelConversation(data))
-    {
+    if (_isChatMessage(data) && _isChannelConversation(data)) {
         var naughty = _getNaughtyWords(data);
         if (naughty.length > 0) {
-            console.log("naughty naughty: " + naughty);
+            for(var i = 0; i < naughty.length; i++) {
+                _updateWordList(naughty[i]);
+            }
         }
     }
 });
