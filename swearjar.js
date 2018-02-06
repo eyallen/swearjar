9b1ea484493e391d086014d215e6793645059298
Alexander Allen <alexander.ryan.allen@gmail.com>
2018-02-05 21:58:53 -0800

    messing around

--- swearjar.js
+++ swearjar.js
@@ -1,2 +1,40 @@
-var bot = require('slackbots');
-var profanity = require('profanity-util');
+var SlackBot = require('slackbots');
+var Profanity = require('profanity-util');
+
+var settings = {
+    token: process.env.BOT_API_KEY,
+    name: "Swear Jar"
+};
+
+var bot = new SlackBot(settings);
+
+// TODO: Move these to a prototype or something
+_isChatMessage = function (message) {
+    return message.type === 'message' && Boolean(message.text);
+};
+
+_isChannelConversation = function (message) {
+    return typeof message.channel === 'string' &&
+        message.channel[0] === 'C';
+};
+
+_getNaughtyWords = function(message) {
+    return Profanity.check(message.text);
+};
+
+bot.on('start', function()
+{
+    bot.postMessageToChannel("swearjartest", "its on");
+});
+
+bot.on('message', function(data) {
+    console.log(data);
+
+    if (_isChatMessage(data) && _isChannelConversation(data))
+    {
+        var naughty = _getNaughtyWords(data);
+        if (naughty.length > 0) {
+            console.log("naughty naughty: " + naughty);
+        }
+    }
+});
